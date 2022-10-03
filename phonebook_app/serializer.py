b1853f7284eba21b6b6ff26b8876b96ae9f5b23c
Ivy Kabere <kabereivy@gmail.com>
2022-10-04 00:32:48 +0300

    create phonebook get APIview

---
+++ phonebook_app/serializer.py
@@ -0,0 +1,8 @@
+from .models import Phonebook
+from rest_framework import serializers
+
+
+class PhonebookSerializer(serializers.ModelSerializer):
+  class Meta:
+    model=Phonebook
+    fields='__all__'
--- phonebook_app/views.py
+++ phonebook_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from phonebook_app.serializer import PhonebookSerializer
+from rest_framework.response import Response
 
 # Create your views here.
+class Phonebook(APIView):
+  def get(self,request,format=None()):
+    all_contacts=Phonebook.objects.all()
+    serializer=PhonebookSerializer(all_contacts,many=True)
+    return Response(serializer.data)
