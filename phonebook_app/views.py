220eb5fb0806b82b2131ab8faa2758cad79a4bdf
Ivy Kabere <kabereivy@gmail.com>
2022-10-04 00:44:28 +0300

    create phonebook post view

--- phonebook_app/views.py
+++ phonebook_app/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from phonebook_app.serializer import PhonebookSerializer
 from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 class Phonebook(APIView):
-  def get(self,request,format=None()):
+  def get(self,request,format=None):
     all_contacts=Phonebook.objects.all()
     serializer=PhonebookSerializer(all_contacts,many=True)
     return Response(serializer.data)
+
+  def post(self,request,format=None):
+    serializer=PhonebookSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data,status.HTTP_201_CREATED)
+    return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
